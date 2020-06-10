1d7c3369f1c6edeb1a253286ec60f923ed3caa01
FCTESLA <fcamacho888@gmail.com>
2020-06-09 22:30:15 -0700

    working routes

--- server.js
+++ server.js
@@ -35,29 +35,29 @@ app.get("/stats", (req, res) => {
 app.get("/api/workouts", (req, res) => {
 
   db.Workout.find({})
-      .then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-      .catch(err => { 
-          res.json(err);
-      });
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 app.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-  .then((dbWorkout) => {
-    res.json(dbWorkout);
-  })
-  .catch((err) => {
-    res.json(err);
-  });
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.put("/api/workouts/:id", (req, res) => {
   db.Workout.update(
-    {_id: req.params.id},
-    {$push: {exercises: req.body}}
-    )
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -69,7 +69,7 @@ app.put("/api/workouts/:id", (req, res) => {
 app.get("/api/workouts/range", (req, res) => {
   let date = new Date();
 
-  db.Workout.find({day : {$gte: new Date().setDate(new Date().getDate()-7)}})
+  db.Workout.find({ day: { $gte: new Date().setDate(new Date().getDate() - 7) } })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
